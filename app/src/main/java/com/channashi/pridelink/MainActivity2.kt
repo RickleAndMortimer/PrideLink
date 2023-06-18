88f3fdee3aa6034aa9d7072567c815388679e73f
channacy <68759286+channacy@users.noreply.github.com>
2023-06-17 22:51:53 -0400

    nav bar?

--- app/src/main/java/com/channashi/pridelink/MainActivity2.kt
+++ app/src/main/java/com/channashi/pridelink/MainActivity2.kt
@@ -2,10 +2,46 @@ package com.channashi.pridelink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.channashi.pridelink.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(Home())
+                R.id.profile -> replaceFragment(Profile())
+                R.id.studio -> replaceFragment(Studio())
+                R.id.journal -> replaceFragment(Journal())
+
+                else ->{
+
+
+
+                }
+
+            }
+
+            true
+
+        }
     }
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+
+    }
+
 }
